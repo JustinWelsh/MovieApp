2363c5c264434f7135ba344ebf9d9246ca8c5030
Justin Welsh <justinwelsh33@gmail.com>
2026-06-11 19:27:14 -0500

    refactor: extract GENRE_MAP to shared _config/genres.js

--- src/components/card/MovieCard.jsx
+++ src/components/card/MovieCard.jsx
@@ -4,28 +4,7 @@ import { Image } from "@nextui-org/image";
 import { motion, AnimatePresence } from "framer-motion";
 import { fadeInUp30 } from "../../_config/animations";
 import { useWatchlistContext } from "../../context/WatchlistContext";
-
-const GENRE_MAP = {
-  28: "Action",
-  12: "Adventure",
-  16: "Animation",
-  35: "Comedy",
-  80: "Crime",
-  99: "Documentary",
-  18: "Drama",
-  10751: "Family",
-  14: "Fantasy",
-  36: "History",
-  27: "Horror",
-  10402: "Music",
-  9648: "Mystery",
-  10749: "Romance",
-  878: "Sci-Fi",
-  10770: "TV Movie",
-  53: "Thriller",
-  10752: "War",
-  37: "Western",
-};
+import { GENRE_MAP } from "../../_config/genres";
 
 const OVERLAY_WIDTH = 420;
 
